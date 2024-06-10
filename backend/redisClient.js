092232a9ffdf3b10b3b95bcaa54d04aa075c53dd
atef fawaz <atefawaz@gmail.com>
2024-06-10 18:04:26 +0200

    added redis

---
+++ backend/redisClient.js
@@ -0,0 +1,14 @@
+// redisClient.js
+import { createClient } from 'redis';
+
+const redisClient = createClient({
+  url: 'redis://localhost:6379'
+});
+
+redisClient.on('error', (err) => {
+  console.log('Redis Client Error', err);
+});
+
+await redisClient.connect();
+
+export default redisClient;
